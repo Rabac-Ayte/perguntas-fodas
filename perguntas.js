9114e5a1399833a2dd122b7083539043fd019fb0
Cicada 3301 <114597539+Rabac-Ayte@users.noreply.github.com>
2024-11-05 07:48:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,69 @@
+criaCartao(
+    'Filosofia',
+    'Qual a primeira via eleata do saber?',
+    'A verdade absoluta, definida na existência do ser e da não-existência do não ser'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual o livro onde o homem é feito de corno e vai atrás da sua mulher travando uma guerra em outra cidade?',
+    'A Ilíada'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual o nome do método onde ocorre o conflito de ideias?',
+    'Dialético'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual o nome da imagem mental que criamos de um objeto quando ele sai da nossa percepção sensível?',
+    'Phantasma'
+)
+
+criaCartao(
+    'Sociologia',
+    'Qual os dois tipos de sanções sociais definidas por Durkheim?',
+    'A sanção legal e a sanção espontânea'
+)
+criaCartao(
+    'Literatura',
+    'Nome da peça do shakespeare onde tem 3 bruxas dançando no inicio da peça?',
+    'Macbeth'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual o problema da filosofia em Hume?',
+    'A de inexistência das causas'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual o ultimo nível de amor para Platão?',
+    'O amor ao verdadeiro em si'
+)
+
+criaCartao(
+    'Literatura',
+    'Nome do livro onde um monte de homem se junta em um barco pra matar um animal 50 vezes maior que eles com uns arpãos',
+    'Moby Dick'
+)
+
+criaCartao(
+    'Literatura',
+    'Nome do livro onde está o conto de uma biblioteca infinita com todas as combinações de palavras possíveis',
+    'Ficções, do Borges.'
+)
+
+criaCartao(
+    'Literatura',
+    'Nome do livro onde um homem solitário mata uma velha',
+    'Crime e Castigo'
+)
+criaCartao(
+    'Literatura',
+    'Nome da peça onde um rei é assassinado pela própria mulher',
+    'Agamenão'
+)
